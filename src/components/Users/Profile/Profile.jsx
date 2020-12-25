01e09b4d9178249424cb55660b607588c5cdcf89
Antanidoss <antosci2002@gmail.com>
2020-12-25 20:44:43 +0300

    added style for logout page

--- src/components/Users/Profile/Profile.jsx
+++ src/components/Users/Profile/Profile.jsx
@@ -1,13 +1,14 @@
 import React from "react"
+import classes from "./Profile.module.css"
 
 let Profile = (props) => {
     debugger
     return (
-        <div>
-            <div>
+        <div className={classes.profile}>
+            <div className={classes.name}>
                 {props.userName}
             </div>
-            <div>
+            <div className={classes.logoutButton}>
                 <button onClick={props.logout}>Выйти</button>
             </div>
         </div>
